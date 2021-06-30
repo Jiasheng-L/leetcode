04cc198cf8b38fd48b9c8cb682a023fa9c284181
jiasheng <1067460646@qq.com>
2021-06-30 20:45:19 +0800

    update

--- 1.twoSum/twoSum.c
+++ 1.twoSum/twoSum.c
@@ -10,10 +10,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     *returnSize = 0;
     for (i = 0; i < numsSize; i++)
     {
-        if (nums[i] >= target)
-        {
-            continue;
-        }
         for (j=i+1; j<numsSize; j++)
         {
             if (*(nums+i) + *(nums+j) == target)
